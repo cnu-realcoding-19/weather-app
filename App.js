9bfc87bb4d91321c349896fe688687d0fa88ab0f
Kim-AYoung <onionlily123@naver.com>
2021-06-01 12:51:22 +0900

    09. Props and states

--- App.js
+++ App.js
@@ -3,24 +3,29 @@ import React from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 
 class CounterButton extends React.Component {
+  constructor(props) {
+    const { counter } = props;
+    super(props);
+
+    this.state = {
+      counter,
+    };
+  }
+
   state = {
     counter: 0,
   };
 
-  clickHandler = () => {
-    this.setState({
+  clickHandler = () => this.setState({
       counter: this.state.counter + 1,
-    });
-  };
+  });
 
-  render() {
-    return (
+  render = () => (
       <View style={styles.container}>
         <Text style={styles.counter}>{this.state.counter}</Text>
         <Button title={'click me!'} onPress={this.clickHandler}/>
       </View>
-    );
-  }
+  );
 }
 
 export default class App extends React.Component {
@@ -28,10 +33,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton />
-        <CounterButton />
-        <CounterButton />
-        <CounterButton />
+        <CounterButton counter={0} />
+        <CounterButton counter={1} />
+        <CounterButton counter={2} />
+        <CounterButton counter={3} />
         <StatusBar style="auto" />
       </View>
     );
