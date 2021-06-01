f44b5a36ed62e4f136f04dd8395543234885c36b
Kim-AYoung <onionlily123@naver.com>
2021-06-01 13:35:23 +0900

    19. ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, ScrollView, Image, Text } from 'react-native';
 
 // after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,8 +9,8 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <SafeAreaView style={styles.fullscreen}>
-        <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
           <Text style={{fontSize:96}}>Scroll me plz</Text>
           <Image source={{
               uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
@@ -46,26 +46,16 @@ export default class App extends React.Component {
               uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
               width: 128,
               height: 128,
-          }} />
-          <StatusBar style="auto" />
-        </View>
+            }} />
+        </ScrollView>
+        <StatusBar style="auto" />
       </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  fullscreen: {
-    flex: 1,
-  },
   container: {
     flex: 1,
-
-    backgroundColor: '#fff',
-    borderColor: 'red',
-    borderWidth: 10,
-
-    // justifyContent: 'center',
-    alignItems: 'center',
   },
 });
