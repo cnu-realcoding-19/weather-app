59fcfdf868d7dba29c68534f83a67d11d3d27a42
OMEGA-Y <science_co@naver.com>
2021-06-01 18:51:15 +0900

    26. Fetching cities and rendering them

--- App.js
+++ App.js
@@ -6,30 +6,41 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cities: [],
+    };
+  }
+
   componentDidMount() {
     fetch(
       "https://raw.githubusercontent.com/example0312/weather-crawler/e3168f2b4e316691f8ab385f738783976eef7f0d/availableCityNames"
     )
       .then((response) => response.json())
-      .then(console.log);
+      .then((cities) => {
+        console.log("cities =", cities.length);
+        this.setState({
+          cities,
+        });
+      });
   }
 
-  renderItem({ name }) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.text}>{name}</Text>
-      </View>
-    );
-  }
+  renderItem = (city) => (
+    <View style={styles.item}>
+      <Text style={styles.text}>{city}</Text>
+    </View>
+  );
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
           style={styles.container}
-          keyExtractor={(item) => item.name}
+          keyExtractor={(item) => item}
           renderItem={({ item }) => this.renderItem(item)}
-          data={this.fruits}
+          data={this.state.cities}
         />
 
         <StatusBar style="auto" />
