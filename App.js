a9a7e0d02724dfb1043b57227b0b48ec5d243b5e
Kim-AYoung <onionlily123@naver.com>
2021-06-01 13:05:02 +0900

    13. without flex-box

--- App.js
+++ App.js
@@ -2,43 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 
-class CounterButton extends React.Component {
-  constructor(props) {
-    const { 
-      counter = 0
-    } = props;
-    super(props);
-
-    this.state = {
-      counter,
-    };
-  }
-
-  state = {
-    counter: 0,
-  };
-
-  clickHandler = () => this.setState({
-      counter: this.state.counter + 1,
-  });
-
-  render = () => (
-      <View style={styles.buttonContainer}>
-        <Text style={styles.counter}>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={this.clickHandler}/>
-      </View>
-  );
-}
-
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton />
-        <CounterButton counter={1} />
-        <CounterButton counter={2} />
-        <CounterButton counter={3} />
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
         <StatusBar style="auto" />
       </View>
     );
@@ -51,16 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
-  counter: {
-    fontSize: 30,
-    paddingBottom: 20,
-  },
-  buttonContainer: {
-    alignItems: 'center',
-    width: '100%',
-
+  text: {
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: 'red',
   },
 });
