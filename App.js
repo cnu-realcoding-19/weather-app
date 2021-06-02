b065f35a47f9dbd622c5741c77673d8c0a7c5979
Yami <siontama@gmail.com>
2021-06-02 16:06:38 +0900

    32. Add the weather detail screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 import CityList from './CityList';
+import WeatherDetailScreen from './WeatherDetailScreen';
 
 const AppNavigator = createStackNavigator({
     Home: {
